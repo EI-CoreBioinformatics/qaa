ad3b7918a813358d7fa8faa0c2bde702a62f3c2d
Christian Schudoma (cschu) <christian.schudoma@earlham.ac.uk>
2018-02-01 17:27:07 +0000

    Added QAA_Runner class.

--- qaa/__init__.py
+++ qaa/__init__.py
@@ -15,6 +15,8 @@ DEFAULT_CONFIG_FILE = os.path.join(ETC_DIR, "qaa_config.yaml")
 import csv
 from collections import namedtuple
 
+import yaml
+
 QAA_SAMPLESHEET_COLUMNS = "id assembly bamfile r1 r2 busco_id transcripts proteins".split(" ")
 QAA_Sample = namedtuple("QAA_Sample", QAA_SAMPLESHEET_COLUMNS)
 
@@ -42,3 +44,79 @@ def loadPreCmd(command, is_dependency=True):
                                 return " {} ".format(cc)
 
         return ""
+
+from eicore.external_process.snakemake_helper import *
+
+class QAA_Runner(object):
+	def __init__(self, qaa_args):
+		print("Configuring execution environment ... ", end="", flush=True)
+		self.output_dir = qaa_args.output_dir
+		self.logs_dir = os.path.join(self.output_dir, "hpc_logs")
+		self.exe_env = ExecutionEnvironment(qaa_args, NOW, job_suffix=qaa_args.input + "_" + self.output_dir, log_dir=self.logs_dir)
+		print("done.")
+		print(str(self.exe_env))
+		print()
+
+		if qaa_args.config:
+			print("Custom configuration file specified, overriding defaults ... ", end="", flush=True)
+			self.config = yaml.load(open(qaa_args.config))
+			self.config_file = qaa_args.config
+			print("done.")
+			print()
+		else:
+			print("Loading default configuration ... ", end="", flush=True)
+			self.config = yaml.load(open(DEFAULT_CONFIG_FILE))
+			self.config_file = DEFAULT_CONFIG_FILE
+			print("done.")
+			print()
+
+		if os.path.exists(self.output_dir):
+			print("Output directory already exists, attempting to resume ...")
+		else:
+			print("Output directory does not exist. Creating ... ", end="", flush=True)
+			os.makedirs(self.output_dir)
+			print("done.")
+
+		if not os.path.exists(self.logs_dir) and self.exe_env.use_scheduler:
+			print("HPC log dir does not exist. Creating ... ", end="", flush=True)
+			os.makedirs(self.logs_dir)
+			print("done.")
+
+		print()
+			
+		self.config["samplesheet"] = qaa_args.input
+		self.config["out_dir"] = self.output_dir
+		self.config["etc"] = os.path.join(os.path.dirname(__file__), "..", "etc")
+		self.config["cwd"] = os.getcwd()
+
+		self.config["blobtools_run_bwa"] = not qaa_args.blobtools_no_bwa
+		requested_modes = qaa_args.qaa_mode.split(",")
+		modes = ("geno", "genome", "tran", "transcriptome", "prot", "proteome")
+		invalid_modes = list(filter(lambda s:s not in modes, requested_modes))
+		if invalid_modes:
+			valid_modes = list(set(requested_modes).difference(invalid_modes))
+			if valid_modes:
+				print("--qaa-mode: Found invalid modes {}. Proceeding with {}.".format(invalid_modes, valid_modes))
+				requested_modes = valid_modes
+			else:
+				print("--qaa-mode: No valid mode provided. Valid modes are {}. Exiting.".format(",".join(modes)))
+				sys.exit(1)
+
+		self.config["survey_assembly"] = qaa_args.survey_assembly
+		self.config["run_genome_module"] = qaa_args.survey_assembly or ("geno" in requested_modes or "genome" in requested_modes)
+		self.config["run_transcriptome_module"] = not qaa_args.survey_assembly and ("tran" in requested_modes or "transcriptome" in requested_modes)
+		self.config["run_proteome_module"] = not qaa_args.survey_assembly and ("prot" in requested_modes or "proteome" in requested_modes)
+
+		self.new_config_file = os.path.join(self.output_dir, "qaa.conf.xml")
+		with open(self.new_config_file, "w") as conf_out:
+			yaml.dump(self.config, conf_out, default_flow_style=False)
+
+		self.unlock = qaa_args.unlock
+
+	def run(self):		
+		return run_snakemake(os.path.join(os.path.dirname(__file__), "zzz", "qaa.smk.py"), self.output_dir, self.new_config_file, self.exe_env, dryrun=False, unlock=self.unlock)
+	
+
+class QAA_ArgumentsAdapter(object):
+	def __init__(self):
+		pass
--- qaa/__main__.py
+++ qaa/__main__.py
@@ -11,7 +11,7 @@ from snakemake.utils import min_version
 
 from eicore import NOW
 from eicore.external_process.snakemake_helper import *
-from . import DEFAULT_CONFIG_FILE, DEFAULT_HPC_CONFIG_FILE, __version__
+from . import DEFAULT_CONFIG_FILE, DEFAULT_HPC_CONFIG_FILE, __version__, QAA_Runner
 
 min_version("4.0")
 
@@ -77,80 +77,9 @@ def main():
 
 	args = parser.parse_args()
 	print(args)
-	
-	print("Configuring execution environment ... ", end="", flush=True)
-	logs_dir = join(args.output_dir, "hpc_logs")
-	exe_env = ExecutionEnvironment(args, NOW, job_suffix=args.input + "_" + args.output_dir, log_dir=logs_dir)
-	print("done.")
-	print(str(exe_env))
-	print()
 
-	if args.config:
-		print("Custom configuration file specified, overriding defaults ... ", end="", flush=True)
-		config = yaml.load(open(args.config))
-		config_file = args.config
-		print("done.")
-		print()
-	else:
-		print("Loading default configuration ... ", end="", flush=True)
-		config = yaml.load(open(DEFAULT_CONFIG_FILE))
-		config_file = DEFAULT_CONFIG_FILE
-		print("done.")
-		print()
-
-	if os.path.exists(args.output_dir):
-		print("Output directory already exists, attempting to resume...")
-	else:
-		print("Output director does not exist. Creating ... ", end="", flush=True)
-		os.makedirs(args.output_dir)
-		print("done.")
-	
-	if not os.path.exists(logs_dir) and exe_env.use_scheduler:
-		print("HPC log dir does not exist. Creating ... ", end="", flush=True)
-		os.makedirs(logs_dir)
-		print("done.")
-	
-	print()
-	
-	print(config)
-	config["samplesheet"] = args.input
-	config["out_dir"] = args.output_dir
-	config["etc"] = join(dirname(__file__), "..", "etc")
-	config["cwd"] = os.getcwd()
-	config["blobtools_run_bwa"] = not args.blobtools_no_bwa
-
-	"""if args.contig_minlen:
-        	config["use_asm_lengthfilter"] = True
-	        config["asm_lengthfilter_contig_minlen"] = args.contig_minlen
-	else:
-        	config["use_asm_lengthfilter"] = False
-	        config["asm_lengthfilter_contig_minlen"] = 0
-	"""
-
-	requested_modes = args.qaa_mode.split(",")
-	modes = ("geno", "genome", "tran", "transcriptome", "prot", "proteome")
-	invalid_modes = list(filter(lambda s:s not in modes, requested_modes))
-	if invalid_modes:
-		valid_modes = list(set(requested_modes).difference(invalid_modes))
-		if valid_modes:
-			print("--qaa-mode: Found invalid modes {}. Proceeding with {}.".format(invalid_modes, valid_modes))
-			requested_modes = valid_modes
-		else:
-			print("--qaa-mode: No valid mode provided. Valid modes are {}. Exiting.".format(",".join(modes)))
-			sys.exit(1)
-
-	config["survey_assembly"] = args.survey_assembly
-	config["run_genome_module"] = args.survey_assembly or ("geno" in requested_modes or "genome" in requested_modes)
-	config["run_transcriptome_module"] = not args.survey_assembly and ("tran" in requested_modes or "transcriptome" in requested_modes)
-	config["run_proteome_module"] = not args.survey_assembly and ("prot" in requested_modes or "proteome" in requested_modes)
-
-	new_config_file = join(args.output_dir, "qaa.conf.xml")
-	with open(new_config_file, "w") as conf_out:
-		yaml.dump(config, conf_out, default_flow_style=False)
-	
-	run_result = run_snakemake(join(dirname(__file__), "zzz", "qaa.smk.py"), args.output_dir, new_config_file, exe_env, dryrun=False, unlock=args.unlock)
-	
-		
+	qaa_runner = QAA_Runner(args)
+	run_result = qaa_runner.run()
 
 if __name__ == "__main__":
 	main()
